3048ef118532a57a14ea42037125a2896a827a31
aungkhantmyat-23 <aungkhantmyatroger@gmail.com>
2022-01-23 22:11:56 +0630

    .

--- src/app/service/group.service.ts
+++ src/app/service/group.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, pluck } from 'rxjs';
-import { Hero } from '../models/heros';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HerosService {
+export class Group {
   constructor(private http: HttpClient) {}
-  getHeros(): Observable<any> {
-    return this.http.get<Hero[]>('assets/db.json').pipe(pluck('heros'));
+
+  getGroups(): Observable<any> {
+    return this.http.get<Group[]>('assets/db.json').pipe(pluck('groups'));
   }
 }
